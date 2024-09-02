c5b5dc9d64e2ba259bd747c916bce208e20d9898
hasnocool <101779643+hasnocool@users.noreply.github.com>
2024-09-02 04:18:10 -0600

    Update changes.

--- build_script_v1.py
+++ build_script_v1.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Ollama configuration
-OLLAMA_API_URL = "http://192.168.1.223:11434"
+OLLAMA_API_URL = "http://127.0.0.1:11434"
 OLLAMA_MODEL = 'phi3'
 
 def run_command(command):
